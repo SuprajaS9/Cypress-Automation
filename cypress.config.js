c6f5b732c16678d9daf96cd7bff21c3fb0940f3d
SuprajaS9 <112849948+SuprajaS9@users.noreply.github.com>
2022-09-10 18:39:35 +0530

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,12 @@ const { defineConfig } = require("cypress");
 //registerCypressGrep()
 
 module.exports = defineConfig({
+  projectId: "e57qhk",
 
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      
       require('cypress-grep/src/plugin')(config);
       return config;
     },
