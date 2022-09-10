efe4c0835144bd0f2cea8c9172439cd3e47c9c01
SuprajaS9 <112849948+SuprajaS9@users.noreply.github.com>
2022-09-10 18:36:08 +0530

    Initial commit

---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+//const registerCypressGrep = require('cypress-grep')
+//registerCypressGrep()
+
+module.exports = defineConfig({
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      require('cypress-grep/src/plugin')(config);
+      return config;
+    },
+  },
+});
+
+/*const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    // e2e options here
+  }
+})*/
